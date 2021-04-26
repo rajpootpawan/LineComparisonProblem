19daca6e00313e7899000aaac5739ed2969108bd
pawan singh <kushpawal1@gmail.com>
2021-04-26 08:02:07 +0530

    [pawan] added Welcome line in the LineComparision.java

---
+++ LineComparision.java
@@ -0,0 +1,10 @@
+class LineComparision
+{
+		public static void main(String []args)
+         	{
+                	//Master Added code for Welcome line..
+                	System.out.println("Welcome to Line Computation Program on Master Branch");
+
+		}
+
+}
